54b64f4d9346a0702db88a25ce3a82738c0099e7
Scott Olmsted <solmsted@gmail.com>
2016-11-30 14:05:02 -0500

    Fix bugs

--- versions.rb
+++ versions.rb
@@ -5,18 +5,19 @@ require 'fileutils'
 
 DAY_IN_SECS = 24.0 * 60.0 * 60.0
 TWO_HOURS_IN_SECS = 2.0 * 60.0 * 60.0
-TEN_MINS_IN_SECS = 10.0 #* 60.0
+INTERVAL_IN_SECS = 60.0 #* 10.0 #* 60.0
 
 Dir.mkdir('./Versions') unless Dir.exists?('./Versions')
 
 def versions
   files = Dir.glob("./**/*.{doc,docx,xls,xlsx,ods,odt,csv,txt,rtf,psd,rb}")
+
   files.each do |filepath|
     # skip backup files
     next if filepath.include?('Versions')
 
     # skip file if not modified recently
-    next if Time.now - File.mtime(filepath) > TEN_MINS_IN_SECS
+    next if Time.now - File.mtime(filepath) > INTERVAL_IN_SECS
 
     # split filepath into name and path
     name = File.basename(filepath)
@@ -24,26 +25,26 @@ def versions
 
     # skip this file
     next if name == 'versions' || name == 'versions.rb'
-    
-    #binding.pry
-    puts "  filepath=#{filepath.inspect}"
-    puts "  path=#{path}"
-    puts "  name=#{name}"
 
     # make sure /Versions directory exists
     versions_dir = "./#{path}/Versions"
     Dir.mkdir(versions_dir) unless Dir.exists?(versions_dir)
 
+    # create versioned filename
+    prefix = Time.now.strftime('%Y.%m.%d %H.%M')
+    version_path = "#{versions_dir[2..versions_dir.size-1]}/#{prefix}-#{name}"
+
     # copy file
-    FileUtils.cp(filepath, "#{versions_dir}/#{name}")
-    puts "  Version saved: #{filepath}"
+    FileUtils.cp(filepath, version_path)
+
+    # tell the world
+    puts "#{prefix}-#{name}"
   end
 end
 
 # main loop
+puts "Versions started..."
 loop do
   versions
-  puts 'Versions completed at ' + Time.now.strftime('%A, %d %b %Y %l:%M %p')
-  puts '================================'
-  sleep(TEN_MINS_IN_SECS)
+  sleep(INTERVAL_IN_SECS)
 end
