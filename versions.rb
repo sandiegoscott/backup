b0e0f852520702d61933cafd1424cb3ba5678fb6
Scott Olmsted <solmsted@gmail.com>
2016-12-01 21:27:47 -0500

    Fix a few more small bugs

--- versions.rb
+++ versions.rb
@@ -5,9 +5,7 @@ require 'fileutils'
 
 DAY_IN_SECS = 24.0 * 60.0 * 60.0
 TWO_HOURS_IN_SECS = 2.0 * 60.0 * 60.0
-INTERVAL_IN_SECS = 60.0 #* 10.0 #* 60.0
-
-Dir.mkdir('./Versions') unless Dir.exists?('./Versions')
+INTERVAL_IN_SECS = 5.0 * 60.0
 
 def versions
   files = Dir.glob("./**/*.{doc,docx,xls,xlsx,ods,odt,csv,txt,rtf,psd,rb}")
@@ -27,7 +25,7 @@ def versions
     next if name == 'versions' || name == 'versions.rb'
 
     # make sure /Versions directory exists
-    versions_dir = "./#{path}/Versions"
+    versions_dir = "./#{path}/Versions".gsub('//', '/')
     Dir.mkdir(versions_dir) unless Dir.exists?(versions_dir)
 
     # create versioned filename
@@ -35,10 +33,10 @@ def versions
     version_path = "#{versions_dir[2..versions_dir.size-1]}/#{prefix}-#{name}"
 
     # copy file
-    FileUtils.cp(filepath, version_path)
+    FileUtils.cp(filepath, "./#{version_path}")
 
     # tell the world
-    puts "#{prefix}-#{name}"
+    puts version_path
   end
 end
 
